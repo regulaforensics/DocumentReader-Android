a1d73b314fc75f441809829e25a535991e9e134c
Vladislav Yakimchik <vyakimchik8@gmail.com>
2020-05-13 17:03:43 +0300

    Updated sample to the latest SDK

--- Samples/DocumentReader-sample/app/src/main/java/com/regula/documentreader/MainActivity.java
+++ Samples/DocumentReader-sample/app/src/main/java/com/regula/documentreader/MainActivity.java
@@ -30,6 +30,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.regula.documentreader.api.DocumentReader;
+import com.regula.documentreader.api.completions.IDocumentReaderCompletion;
+import com.regula.documentreader.api.completions.IDocumentReaderInitCompletion;
+import com.regula.documentreader.api.completions.IDocumentReaderPrepareCompletion;
 import com.regula.documentreader.api.enums.DocReaderAction;
 import com.regula.documentreader.api.enums.eGraphicFieldType;
 import com.regula.documentreader.api.enums.eRFID_Password_Type;
@@ -179,7 +182,7 @@ public class MainActivity extends AppCompatActivity {
 
             //preparing database files, it will be downloaded from network only one time and stored on user device
             DocumentReader.Instance().prepareDatabase(MainActivity.this, "Full", new
-                    DocumentReader.DocumentReaderPrepareCompletion() {
+                    IDocumentReaderPrepareCompletion() {
                         @Override
                         public void onPrepareProgressChanged(int progress) {
                             initDialog.setTitle("Downloading database: " + progress + "%");
@@ -189,14 +192,14 @@ public class MainActivity extends AppCompatActivity {
                         public void onPrepareCompleted(boolean status, String error) {
 
                             //Initializing the reader
-                            DocumentReader.Instance().initializeReader(MainActivity.this, license, new DocumentReader.DocumentReaderInitCompletion() {
+                            DocumentReader.Instance().initializeReader(MainActivity.this, license, new IDocumentReaderInitCompletion() {
                                 @Override
                                 public void onInitCompleted(boolean success, String error) {
                                     if (initDialog.isShowing()) {
                                         initDialog.dismiss();
                                     }
 
-                                    DocumentReader.Instance().customization().setShowHelpAnimation(false);
+                                    DocumentReader.Instance().customization().edit().setShowHelpAnimation(false).apply();
 
                                     //initialization successful
                                     if (success) {
@@ -206,7 +209,7 @@ public class MainActivity extends AppCompatActivity {
                                                 clearResults();
 
                                                 //starting video processing
-                                                DocumentReader.Instance().showScanner(completion);
+                                                DocumentReader.Instance().showScanner(MainActivity.this, completion);
                                             }
                                         });
 
@@ -297,7 +300,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //DocumentReader processing callback
-    private DocumentReader.DocumentReaderCompletion completion = new DocumentReader.DocumentReaderCompletion() {
+    private IDocumentReaderCompletion completion = new IDocumentReaderCompletion() {
         @Override
         public void onCompleted(int action, DocumentReaderResults results, String error) {
             //processing is finished, all results are ready
@@ -321,7 +324,7 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     //starting chip reading
-                    DocumentReader.Instance().startRFIDReader(new DocumentReader.DocumentReaderCompletion() {
+                    DocumentReader.Instance().startRFIDReader(MainActivity.this, new IDocumentReaderCompletion() {
                         @Override
                         public void onCompleted(int rfidAction, DocumentReaderResults results, String error) {
                             if (rfidAction == DocReaderAction.COMPLETE || rfidAction == DocReaderAction.CANCEL) {
