0da4e195a0f60b7e9a2b2c7dabf2721af249e5d4
Sergey Yakimchik <yakimchik7@gmail.com>
2021-08-31 15:36:16 +0300

    migrate to androidX

--- Samples/Basic/DocumentReader-sample_kotlin/app/src/main/java/com/regula/documentreader/MainActivity.kt
+++ Samples/Basic/DocumentReader-sample_kotlin/app/src/main/java/com/regula/documentreader/MainActivity.kt
@@ -12,10 +12,10 @@ import android.graphics.BitmapFactory.decodeStream
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
